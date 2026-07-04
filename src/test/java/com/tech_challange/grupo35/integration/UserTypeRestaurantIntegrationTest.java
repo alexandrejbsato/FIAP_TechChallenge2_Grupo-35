01667274966d13c2f22fe762a76d4eebc3ff538a
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2026-07-03 23:09:00 -0300

    Corrige import do LocalServerPort para Spring Boot 4

--- src/test/java/com/tech_challange/grupo35/integration/UserTypeRestaurantIntegrationTest.java
+++ src/test/java/com/tech_challange/grupo35/integration/UserTypeRestaurantIntegrationTest.java
@@ -3,7 +3,7 @@ package com.tech_challange.grupo35.integration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.*;
 
